24007d59790869785b7ef437cab3f708cc2dc497
Shivaramsagar3054 <sagarshivaram44@gmail.com>
2024-05-13 08:43:18 +0530

    Add files via upload

---
+++ linearch search.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+	int n,f=0,index;
+	printf("Entere the size of the array : ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Entere the size of the array : ");
+	for(int i=0;i<n;i++){
+	scanf("%d",&arr[i]);
+	}
+	int search;
+	printf("Enter tyhe serach element: ");
+	scanf("%d",&search);
+	for(int i=0;i<n;i++){
+	if(arr[i]==search)
+	{
+		f=1;
+		 index=i;
+		break;
+	}
+	}
+	if(f){
+		printf("Elememt is found in pos %d",index);
+	}
+	else
+	printf("Element is not found");
+
+
+	return 0;
+}
